a121b4ded428bf176d693cc59e22d8e09c39861b
JMITCH18 <ronmitchell1@yahoo.com>
2018-11-08 21:13:18 -0600

    added state changing to contact information

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,15 +2,33 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  // Checks the state and switches bool to the opposite
+  onShowClick = () => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+
   render() {
+    //destructuring pulling values
     const { contact } = this.props;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
-        <h4>{contact.name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {contact.email}</li>
-          <li className="list-group-item">Phone: {contact.phone}</li>
-        </ul>
+        <h4>
+          {contact.name}{" "}
+          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
+
+        {/* Checking the state if true show props if false hide the props */}
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {contact.email}</li>
+            <li className="list-group-item">Phone: {contact.phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
